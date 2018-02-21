f175d3affedb7b8a1d606f1e00fd1076bff5334c
Marco Buzzanca <marco.bzn@gmail.com>
2018-02-21 21:18:53 +0100

    Never die
    Removed $die parameter from checkAjaxReferer,
    checkAdminReferer now throws an AuthException,
    ays method removed.

--- src/NonceValidator.php
+++ src/NonceValidator.php
@@ -4,39 +4,31 @@ namespace Wordpress\Nonce;
 
 class NonceValidator extends NonceHasher {
     public function __construct(
-        $action = - 1, $lifetime = DAY_IN_SECONDS,
+        $action, $lifetime = DAY_IN_SECONDS,
         $user = null, $token = null
     ) {
         parent::__construct( $action, $lifetime, $user, $token );
     }
 
-    public static function ays( $action ) {
-        if ( $action == "log-out" ) {
-            $html        = sprintf(
-                __( 'You are attempting to log out of %s' ),
-                get_bloginfo( 'name' )
+    /**
+     * @throws AuthException
+     */
+    public function checkAdminReferer( $query_arg = "_wpnonce" ) {
+        $admin_url = strtolower( admin_url() );
+        $referer   = strtolower( wp_get_referer() );
+
+        $result = isset( $_REQUEST[ $query_arg ] ) ?
+            $this->verify( $_REQUEST[ $query_arg ] ) : false;
+
+        do_action( 'check_admin_referer', $this->getAction(), $result );
+
+        if ( ! $result && strpos( $referer, $admin_url ) !== 0 ) {
+            throw new AuthException( $this->getAction(),
+                "Referer does not come from an admin URL."
             );
-            $html        .= '</p><p>';
-            $redirect_to = isset( $_REQUEST['redirect_to'] ) ?
-                $_REQUEST['redirect_to'] : '';
-            $html        .= sprintf(
-                __( 'Do you really want to <a href="%s">log out</a>?' ),
-                wp_logout_url( $redirect_to )
-            );
-        } else {
-            $html    = __( 'Are you sure you want to do this?' );
-            $referer = wp_get_referer();
-            if ( $referer ) {
-                $html .= '</p><p>';
-                $html .= sprintf(
-                    '<a href="%s">%s</a>',
-                    esc_url( remove_query_arg( 'updated', $referer ) ),
-                    __( 'Please try again.' )
-                );
-            }
         }
 
-        wp_die( $html, __( 'WordPress Failure Notice' ), 403 );
+        return $result;
     }
 
     public function verify( $nonce ) {
@@ -46,10 +38,10 @@ class NonceValidator extends NonceHasher {
         }
 
         $tick = $this->tick();
-        if ( hash_equals( $this->hash( $tick ), $nonce ) ) {
+        if ( hash_equals( $this->getNonce( $tick ), $nonce ) ) {
             return 1;
         }
-        if ( hash_equals( $this->hash( $tick - 1 ), $nonce ) ) {
+        if ( hash_equals( $this->getNonce( $tick - 1 ), $nonce ) ) {
             return 2;
         }
 
@@ -61,47 +53,7 @@ class NonceValidator extends NonceHasher {
         return false;
     }
 
-    public function checkAdminReferer( $query_arg = "_wpnonce" ) {
-        if ( $this->getAction() == - 1 ) {
-            global $wp_version;
-
-            _doing_it_wrong(
-                __FUNCTION__,
-                __( 'You should specify a nonce action to be verified by ' .
-                    'using the first parameter.' ),
-                $wp_version
-            );
-        }
-
-        $admin_url = strtolower( admin_url() );
-        $referer   = strtolower( wp_get_referer() );
-
-        $result = isset( $_REQUEST[ $query_arg ] ) ?
-            $this->verify( $_REQUEST[ $query_arg ] ) : false;
-
-        do_action( 'check_admin_referer', $this->getAction(), $result );
-
-        if ( ! $result &&
-             ! ( $this->getAction() && strpos( $referer, $admin_url ) === 0 ) ) {
-            self::ays( $this->getAction() );
-            die();
-        }
-
-        return $result;
-    }
-
-    public function checkAjaxReferer( $query_arg = "_wpnonce", $die = true ) {
-        if ( $this->getAction() == - 1 ) {
-            global $wp_version;
-
-            _doing_it_wrong(
-                __FUNCTION__,
-                __( 'You should specify a nonce action to be verified by ' .
-                    'using the first parameter.' ),
-                $wp_version
-            );
-        }
-
+    public function checkAjaxReferer( $query_arg = false ) {
         $nonce = '';
 
         if ( $query_arg && isset( $_REQUEST[ $query_arg ] ) ) {
@@ -114,14 +66,6 @@ class NonceValidator extends NonceHasher {
 
         $result = $this->verify( $nonce );
         do_action( 'check_ajax_referer', $this->getAction(), $result );
-
-        if ( $die && $result === false ) {
-            if ( wp_doing_ajax() ) {
-                wp_die( - 1, 403 );
-            } else {
-                die( '-1' );
-            }
-        }
 
         return $result;
     }
